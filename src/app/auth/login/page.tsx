efce740d656e02c32bff3a9c468e3bb6ba31447f
di-iwan <iwan.lelikov@gmail.com>
2025-12-02 13:31:45 +0300

    feat(auth): redesign login page and add sign-up page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,71 +1,205 @@
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Separator } from '@/components/ui/separator'
+"use client";
 
-import Logo from '@/assets/logo'
-import AuthBackgroundShape from '@/assets/auth-background-shape'
-import LoginForm from '@/components/auth/login-form'
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Logo } from "@/assets/logo";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { FigmaIcon, GithubIcon, InstagramIcon, TwitchIcon, TwitterIcon } from "lucide-react";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
 
 const Login = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <div className='relative flex h-auto min-h-screen items-center justify-center overflow-x-hidden px-4 py-10 sm:px-6 lg:px-8'>
-      <div className='absolute'>
-        <AuthBackgroundShape />
-      </div>
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-full h-full grid lg:grid-cols-2">
+        <div className="relative max-w-sm m-auto w-full flex flex-col items-center p-8 outline-0 sm:outline-2 outline-border/40 dark:outline-border/80 outline-offset-0.5">
+          <div className="max-sm:hidden absolute border-t top-0 inset-x-0 w-[calc(100%+4rem)] -translate-x-8" />
+          <div className="max-sm:hidden absolute border-b bottom-0 inset-x-0 w-[calc(100%+4rem)] -translate-x-8" />
+          <div className="max-sm:hidden absolute border-s left-0 inset-y-0 h-[calc(100%+4rem)] -translate-y-8" />
+          <div className="max-sm:hidden absolute border-e right-0 inset-y-0 h-[calc(100%+4rem)] -translate-y-8" />
 
-      <Card className='z-1 w-full border-none shadow-md sm:max-w-lg'>
-        <CardHeader className='gap-6'>
-          <Logo className='gap-3' />
-
-          <div>
-            <CardTitle className='mb-1.5 text-2xl'>Sign in to Shadcn Studio</CardTitle>
-            <CardDescription className='text-base'>Ship Faster and Focus on Growth.</CardDescription>
-          </div>
-        </CardHeader>
-
-        <CardContent>
-          <p className='text-muted-foreground mb-6'>
-            Login with{' '}
-            <a href='#' className='text-card-foreground hover:underline'>
-              Magic Link
-            </a>
+          <div className="max-sm:hidden absolute border-t -top-1 inset-x-0 w-[calc(100%+3rem)] -translate-x-6" />
+          <div className="max-sm:hidden absolute border-b -bottom-1 inset-x-0 w-[calc(100%+3rem)] -translate-x-6" />
+          <div className="max-sm:hidden absolute border-s -left-1 inset-y-0 h-[calc(100%+3rem)] -translate-y-6" />
+          <div className="max-sm:hidden absolute border-e -right-1 inset-y-0 h-[calc(100%+3rem)] -translate-y-6" />
+          <Logo className="h-9 w-9" />
+          <p className="mt-4 text-xl font-semibold tracking-tight">
+            Войти в Mytro
           </p>
 
-          {/* Quick Login Buttons */}
-          <div className='mb-6 flex flex-wrap gap-4 sm:gap-6'>
-            <Button variant='outline' className='grow'>
-              Login as User
+          <div className="mt-8 flex items-center gap-3">
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded-full h-10 w-10"
+            >
+              <GithubIcon className="h-[18px]! w-[18px]!" />
             </Button>
-            <Button variant='outline' className='grow'>
-              Login as Admin
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded-full h-10 w-10"
+            >
+              <InstagramIcon className="h-[18px]! w-[18px]!" />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded-full h-10 w-10"
+            >
+              <TwitterIcon className="h-[18px]! w-[18px]!" />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded-full h-10 w-10"
+            >
+              <FigmaIcon className="h-[18px]! w-[18px]!" />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded-full h-10 w-10"
+            >
+              <TwitchIcon className="h-[18px]! w-[18px]!" />
             </Button>
           </div>
 
-          {/* Login Form */}
-          <div className='space-y-4'>
-            <LoginForm />
+          <div className="my-7 w-full flex items-center justify-center overflow-hidden">
+            <Separator />
+            <span className="text-sm px-2">ИЛИ</span>
+            <Separator />
+          </div>
 
-            <p className='text-muted-foreground text-center'>
-              New on our platform?{' '}
-              <a href='#' className='text-card-foreground hover:underline'>
-                Create an account
-              </a>
+          <Form {...form}>
+            <form
+              className="w-full space-y-4"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Электронная почта</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="me@example.ru"
+                        className="w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Пароль</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Пароль"
+                        className="w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="mt-4 w-full">
+                Войти
+              </Button>
+            </form>
+          </Form>
+
+          <div className="mt-5 space-y-5">
+            <Link
+              href="#"
+              className="text-sm block underline text-muted-foreground text-center"
+            >
+              Забыли пароль?
+            </Link>
+            <p className="text-sm text-center">
+              Нет аккаунта?
+              <Link href="#" className="ml-1 underline text-muted-foreground">
+                Создать аккаунт
+              </Link>
             </p>
-
-            <div className='flex items-center gap-4'>
-              <Separator className='flex-1' />
-              <p>or</p>
-              <Separator className='flex-1' />
-            </div>
-
-            <Button variant='ghost' className='w-full' asChild>
-              <a href='#'>Sign in with google</a>
-            </Button>
           </div>
-        </CardContent>
-      </Card>
+        </div>
+        <div className="bg-muted hidden lg:block border-l" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+const GoogleLogo = () => (
+  <svg
+    width="1.2em"
+    height="1.2em"
+    id="icon-google"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="inline-block shrink-0 align-sub text-inherit size-lg"
+  >
+    <g clipPath="url(#clip0)">
+      <path
+        d="M15.6823 8.18368C15.6823 7.63986 15.6382 7.0931 15.5442 6.55811H7.99829V9.63876H12.3194C12.1401 10.6323 11.564 11.5113 10.7203 12.0698V14.0687H13.2983C14.8122 12.6753 15.6823 10.6176 15.6823 8.18368Z"
+        fill="#4285F4"
+      ></path>
+      <path
+        d="M7.99812 16C10.1558 16 11.9753 15.2915 13.3011 14.0687L10.7231 12.0698C10.0058 12.5578 9.07988 12.8341 8.00106 12.8341C5.91398 12.8341 4.14436 11.426 3.50942 9.53296H0.849121V11.5936C2.2072 14.295 4.97332 16 7.99812 16Z"
+        fill="#34A853"
+      ></path>
+      <path
+        d="M3.50665 9.53295C3.17154 8.53938 3.17154 7.4635 3.50665 6.46993V4.4093H0.849292C-0.285376 6.66982 -0.285376 9.33306 0.849292 11.5936L3.50665 9.53295Z"
+        fill="#FBBC04"
+      ></path>
+      <path
+        d="M7.99812 3.16589C9.13867 3.14825 10.241 3.57743 11.067 4.36523L13.3511 2.0812C11.9048 0.723121 9.98526 -0.0235266 7.99812 -1.02057e-05C4.97332 -1.02057e-05 2.2072 1.70493 0.849121 4.40932L3.50648 6.46995C4.13848 4.57394 5.91104 3.16589 7.99812 3.16589Z"
+        fill="#EA4335"
+      ></path>
+    </g>
+    <defs>
+      <clipPath id="clip0">
+        <rect width="15.6825" height="16" fill="white"></rect>
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default Login;
